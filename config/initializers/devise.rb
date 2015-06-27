8b9daf490b1bb34051f053a0c67dd637f361d5c4
Jonathan Tapia <jonathan.tapia@crowdint.com>
2015-06-27 12:51:59 -0500

    Add json response for devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,1 +1,6 @@
+Devise.setup do |config|
+  config.http_authenticatable_on_xhr = false
+  config.navigational_formats = ["*/*", :html, :json]
+end
+
 Devise.secret_key = "0e25221d7aa0757a5536c2f0c9a5aae4c9e422f67dc59ed8777e2810e620cf59bbe845b726864ce6f3b36e98f6c2157bd66d"
