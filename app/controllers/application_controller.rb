f19d0d97cc38eada86a3477a0aae615d3f6f6e64
Jonathan Tapia <jonathan.tapia@crowdint.com>
2015-06-29 12:14:53 -0500

    Add quick tweak for login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery
-  skip_before_action :verify_authenticity_token, if: :json_request?
+  protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json' }
+  # skip_before_action :verify_authenticity_token, if: :json_request?
 
-  protected
+  # protected
 
-  def json_request?
-    request.format.json?
-  end
+  # def json_request?
+  #   request.format.json?
+  # end
 end
